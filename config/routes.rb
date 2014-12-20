4a98478bfd920d1d0978d9e46a45123d20bc1e17
ungot <ungot@ukr.net>
2014-12-20 19:10:35 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home', 'static_pages/help', 'static_pages/about', 'static_pages/contact'
+#  get 'static_pages/home', 'static_pages/help', 'static_pages/about', 'static_pages/contact'
 
 resources :users
+resources :sessions, only: [:new, :create, :destroy]
+root 'static_pages#home'
 
 match '/help', to: 'static_pages#help', via: 'get'
 match '/about', to: 'static_pages#about', via: 'get'
 match '/contact', to: 'static_pages#contact', via: 'get'
+
 match '/signup', to: 'users#new', via: 'get'
+match '/signin', to: 'sessions#new', via: 'get'
+match '/signout', to: 'sessions#destroy', via: 'delete'
 
   #get 'static_pages/help'
 
@@ -16,7 +21,7 @@ match '/signup', to: 'users#new', via: 'get'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'static_pages#home'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
